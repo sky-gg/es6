94cd2ed5f88f6bff85367f23b3b744212bff52f1
sky-gg <aaaatie@163.com>
2019-10-11 17:13:43 +0800

    Proxy

---
+++ 08.Proxy/字符串解码编码.js
@@ -0,0 +1,18 @@
+function cipherText(arr) {
+  let str = '';
+  arr.forEach(val => {
+    str += String.fromCharCode(val)
+  })
+  return str
+}
+function plainText(str) {
+  let arr = str.split('')
+  let result = []
+  arr.map(val => {
+    result.push(val.codePointAt())
+  })
+  return result
+}
+let com = (f, g) => x => f(g(x))
+let h = com(cipherText, plainText)
+console.log(h('你好世界'))
