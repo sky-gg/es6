84762219473d2c3058702d0332f27f8ce26c6074
sky-gg <aaaatie@163.com>
2019-10-14 15:12:51 +0800

    module

--- 15.module/api.js
+++ 15.module/api.js
@@ -1,2 +1,11 @@
 const PREFIX = '/api/'
 export const GET_USER_INFO = PREFIX + 'getUserInfo'
+
+
+export let num = 0
+export function addNum() {
+  console.log(this, 'this')//undefined
+  console.log(arguments, 'arguments')//
+
+  num++
+}
--- 15.module/index.js
+++ 15.module/index.js
@@ -1,6 +1,16 @@
-import { GET_USER_INFO } from './api'
+import { GET_USER_INFO, num, addNum } from './api.js'
 console.log(GET_USER_INFO)
 
+console.log(num)
+addNum()
+console.log(num)
+// console.log(arguments)
+// console.log(__filename)
+// console.log(__dirname)
+// console.log(exports)
+// console.log(module)
+// console.log(require)
 
-import h from './util'
+
+import h from './util.js'
 h('哈哈哈哈')
